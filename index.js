7b1963bd0326bc59a28d9ee376d0c01961b6d82d
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-15 16:38:29 +0200

    Tehtava 8.3

--- index.js
+++ index.js
@@ -100,13 +100,20 @@ const typeDefs = `
     author: String!,
     id: String!,
     genres: [String],
+  }
 
+  type Author {
+    name: String!,
+    born: Int!,
+    id: String!,
+    bookCount: Int,
   }
 
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -115,6 +122,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((book) => book.author === root.name).length;
+    },
   },
 };
 
