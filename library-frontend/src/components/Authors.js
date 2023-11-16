f173e04e7881859deb9d7826cb76b128594f5c9a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-16 15:37:40 +0200

    Tehtava 8.11

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,5 +1,57 @@
-import { ALL_AUTHORS } from "../queries";
-import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS, UPDATE_AUTHOR } from "../queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
+
+const SetBirthYear = () => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const updatedAuthorDetails = {
+      name: name,
+      born: Number(born),
+    };
+
+    console.log("Update author");
+    console.log(updatedAuthorDetails);
+
+    updateAuthor({
+      variables: updatedAuthorDetails,
+    });
+
+    setName("");
+    setBorn("");
+  };
+
+  return (
+    <>
+      <h3>Set birthyear</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </>
+  );
+};
 
 const Authors = () => {
   const result = useQuery(ALL_AUTHORS);
@@ -27,6 +79,7 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
+      <SetBirthYear />
     </div>
   );
 };
