6570fe5238c62e637b85ef3ee205a6d063517f2a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-17 09:47:48 +0200

    Luo yhteys tietokantaan

--- library-backend/index.js
+++ library-backend/index.js
@@ -2,6 +2,26 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { v1: uuid } = require("uuid");
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const Author = require("./models/author");
+const Book = require("./models/book");
+
+require("dotenv").config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message);
+  });
+
 let authors = [
   {
     name: "Robert Martin",
@@ -96,11 +116,11 @@ let books = [
 
 const typeDefs = `
   type Book {
-    title: String!,
-    published: Int!,
-    author: String!,
-    id: String!,
-    genres: [String],
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
   }
 
   type Author {
